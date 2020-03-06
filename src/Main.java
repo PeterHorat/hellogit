f4c8c0d2f9c36db61b3752d53838b8d32045fcaf
PeterHorat <peterhorat@hispeed.ch>
2020-03-06 15:12:30 +0100

    Versuch 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hallo Git :-)");
+        System.out.println("Hallo Git :-) es geht weiter");
     }
 }
